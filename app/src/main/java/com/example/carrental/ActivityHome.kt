1658c3bf9704d5caf01edc8a7fbbc2a6d4a9a7b9
MartinHernandezC <a20110340@ceti.mx>
2022-06-14 20:56:46 -0500

    alv

--- app/src/main/java/com/example/carrental/ActivityHome.kt
+++ app/src/main/java/com/example/carrental/ActivityHome.kt
@@ -1,11 +1,15 @@
 package com.example.carrental
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.EditText
 import android.widget.Spinner
+import java.util.*
 
 class ActivityHome : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
@@ -16,5 +20,48 @@ class ActivityHome : AppCompatActivity() {
         val adaptador2 = ArrayAdapter(this, android.R.layout.simple_spinner_item, lista);
         spinner.adapter = adaptador;
         spinner2.adapter = adaptador2;
+        val editText: EditText = findViewById(R.id.etDate1);
+        val et2 = findViewById<EditText>(R.id.etDate2);
+
+
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+
+        editText.setOnClickListener {
+            val dpd = DatePickerDialog(
+                this@ActivityHome,
+                DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+
+                   
+                    editText.setText("" + dayOfMonth + "/" + monthOfYear + "/" + year)
+
+                },
+                year,
+                month,
+                day
+            )
+
+            dpd.show()
+
+        }
+        et2.setOnClickListener{
+            val dpd2 = DatePickerDialog(
+                this@ActivityHome,
+                DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+
+
+                    et2.setText("" + dayOfMonth + "/" + monthOfYear + "/" + year)
+
+                },
+                year,
+                month,
+                day
+            )
+
+            dpd2.show()
+
+        }
     }
 }
