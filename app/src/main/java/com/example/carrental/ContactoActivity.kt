bb74dcb555a57b79d968258b1ad158b5430b8234
Emmanuel kim <ekcg123@gmail.com>
2022-12-09 08:14:11 -0600

    First Commit

--- app/src/main/java/com/example/carrental/ContactoActivity.kt
+++ app/src/main/java/com/example/carrental/ContactoActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.Toast
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
@@ -11,12 +12,22 @@ class ContactoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contacto)
-        var btn = findViewById<Button>(R.id.btne);
+        var btn = findViewById<Button>(R.id.btnSubmit);
+        var check : CheckBox = findViewById(R.id.checkBox)
+
         btn.setOnClickListener{
-            Toast.makeText(this, "Gracias por los comentarios", Toast.LENGTH_SHORT).show()
+            if(check.isChecked){
+                Toast.makeText(this, "Gracias por los comentarios", Toast.LENGTH_SHORT).show()
+                finish()
+            }else{
+                Toast.makeText(this, "ACEPTE LOS TERMINOS Y CONDICIONES Perro", Toast.LENGTH_SHORT).show()
+            }
+
         }
         val barAction = supportActionBar
         barAction?.setDisplayShowHomeEnabled(true)
         barAction?.setIcon(R.drawable.coche)
+
+
     }
 }
